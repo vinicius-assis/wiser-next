a900a9e3293b017ebe554077d0cc66fe5042155a
Vinicius <vinicius.assis@acct.global>
2021-02-22 19:28:17 -0300

    style: :lipstick: Add font
    Import Montserrat fonts from google fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,12 @@ const Home: React.FC = () => {
     <>
       <Head>
         <title>Create Next App</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
   }
 `
