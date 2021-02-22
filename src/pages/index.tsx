549691683d8480d5bd374fe885a53fc3ae82a0b8
Vinicius <vinicius.assis@acct.global>
2021-02-22 18:43:45 -0300

    build: :tada: Configured development environment
    Removed useless files, created configuration files and added dependencies

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello Wiser!</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
